45c2c91231ad1b83d874fc151b4a9ebeb919030a
Deepak <sah.sslpu@gmail.com>
2019-07-01 23:27:10 +0530

    ex-7.2: defined method Write on NewWriter struct

--- ch-07/ex-7.2/ex-7.2.go
+++ ch-07/ex-7.2/ex-7.2.go
@@ -10,5 +10,11 @@ import (
 
 type NewWriter struct {
 	w            io.Writer
-	bytesWritten *int64
+	bytesWritten int64
+}
+
+func (n *NewWriter) Write(p []byte) (int, error) {
+	count, err := n.w.Write(p)
+	n.bytesWritten += int64(count)
+	return count, err
 }
