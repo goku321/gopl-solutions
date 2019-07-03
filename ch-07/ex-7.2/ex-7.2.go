74f8384474446e43de2015976406098c21cad807
Deepak <sah.sslpu@gmail.com>
2019-07-03 23:50:24 +0530

    Completed ex-7.2

--- ch-07/ex-7.2/ex-7.2.go
+++ ch-07/ex-7.2/ex-7.2.go
@@ -6,6 +6,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
 )
 
 type NewWriter struct {
@@ -17,4 +18,15 @@ func (n *NewWriter) Write(p []byte) (int, error) {
 	count, err := n.w.Write(p)
 	n.bytesWritten += int64(count)
 	return count, err
+}
+
+func CountingWriter(w io.Writer) (io.Writer, *int64) {
+	nw := NewWriter{w, 0}
+	return &nw, &nw.bytesWritten
+}
+
+func main() {
+	writer, count := CountingWriter(os.Stdout)
+	fmt.Fprint(writer, "Psychotic\n")
+	fmt.Println(*count)
 }
